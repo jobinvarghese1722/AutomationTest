7735f60325ff4977213e93953974ae5603c4f998
Jobin Varghese <jobinvarghese1987@gmail.com>
2020-04-21 21:08:07 +0530

    first commit

--- src/main/java/pages/Login.java
+++ src/main/java/pages/Login.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-import org.testng.asserts.SoftAssert;
+
 
 import utilities.ExcelUtillity;
 import utilities.PageUtility;
